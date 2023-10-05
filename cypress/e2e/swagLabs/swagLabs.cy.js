0044477d35d82497c93d7c2ac1697a8a7ab0c5e1
Tern Puttawit S <tle_srianu@hotmail.com>
2023-10-05 17:36:10 +0700

    Add fail login case

--- cypress/e2e/swagLabs/swagLabs.cy.js
+++ cypress/e2e/swagLabs/swagLabs.cy.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
 
-describe('Swag Labs E2E', () => {
+describe("Swag Labs E2E", () => {
     beforeEach(() => {
         cy.visit("https://www.saucedemo.com/v1/");
     });
-    it('should be able to login', () => {
+    it("should be able to login", () => {
         //perform actions
         cy.get("#user-name").type("standard_user");
         cy.get("#password").type("secret_sauce");
@@ -12,5 +12,15 @@ describe('Swag Labs E2E', () => {
 
         //assertion
         cy.url().should("contain", "inventory.html");
+    });
+
+    it.only("should fail to login", () => {
+        //perform actions
+        cy.get("#user-name").type("non-standard_user");
+        cy.get("#password").type("wrong_sauce");
+        cy.get("#login-button").click();
+
+        //assertion
+        cy.get('[data-test="error"]').should("be.visible");
     });
 });
