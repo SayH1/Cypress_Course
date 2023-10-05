857e33ba911edf33e0a65c303b8dbaa64d03fae4
Tern Puttawit S <tle_srianu@hotmail.com>
2023-10-05 17:30:12 +0700

    Add login test case

--- cypress/e2e/swagLabs/swagLabs.cy.js
+++ cypress/e2e/swagLabs/swagLabs.cy.js
@@ -5,8 +5,12 @@ describe('Swag Labs E2E', () => {
         cy.visit("https://www.saucedemo.com/v1/");
     });
     it('should be able to login', () => {
+        //perform actions
         cy.get("#user-name").type("standard_user");
         cy.get("#password").type("secret_sauce");
         cy.get("#login-button").click();
+
+        //assertion
+        cy.url().should("contain", "inventory.html");
     });
 });
