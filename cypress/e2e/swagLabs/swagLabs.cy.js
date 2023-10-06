9745220f05200a42e225de9e570ed653c3ec6d0d
Tern Puttawit S <tle_srianu@hotmail.com>
2023-10-06 16:48:06 +0700

    Add E2E case with calculation validation

--- cypress/e2e/swagLabs/swagLabs.cy.js
+++ cypress/e2e/swagLabs/swagLabs.cy.js
@@ -49,4 +49,50 @@ describe("Swag Labs E2E", () => {
         //assertion
         cy.get(".inventory_item").eq(0).find(".inventory_item_name").should("have.text", "Test.allTheThings() T-Shirt (Red)");
     });
+
+    it("should be able to checkout successfully", () => {
+        //perform actions
+        cy.get("#user-name").type("standard_user");
+        cy.get("#password").type("secret_sauce");
+        cy.get("#login-button").click();
+        //Inventory page
+        cy.contains("ADD TO CART").eq(0).click();
+        cy.get("#shopping_cart_container").click();
+        cy.get(".checkout_button").click();
+        //Your Information page
+        cy.get("#first-name").type("Firstname1234!@#$");
+        cy.get("#last-name").type("Lastname1234!@#$");
+        cy.get("#postal-code").type("asdfb12345");
+        cy.get(".cart_button").click();
+        //Checkout Overview page
+        //What if we want a generic E2E that always add the 1st available product
+        //and check if it calculates the fee correctly or not?
+        let subTotal = 0;
+        let tax = 0;
+        let total = 0;
+        cy.get(".summary_subtotal_label").then(($ele) => {
+            let split = $ele.text().split(": $");
+            subTotal = split[1];
+            cy.log("Split: " + split);
+            cy.log("Sub Total: " + subTotal);
+        });
+        cy.get(".summary_tax_label").then(($ele) => {
+            let split = $ele.text().split(": $");
+            tax = split[1];
+            cy.log("Split: " + split);
+            cy.log("Tax: " + tax);
+        });
+        cy.get(".summary_total_label").then(($ele) => {
+            let split = $ele.text().split(": $");
+            total = split[1];
+            cy.log("Split: " + split);
+            cy.log("Total: " + total);
+        });
+        cy.then(() => {
+            cy.wrap(+total).should("eq", +subTotal + +tax);
+        });
+        //assertion
+        cy.get(".cart_button").click();
+        cy.get(".complete-header").should("be.visible");
+    });
 });
